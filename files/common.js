10273fb8465b4646e2a2a0c66b0b76132c81a0a6
Oleksandr07 <Oleksandr07@mail.ru>
2016-03-02 17:41:25 +0200

    update
    add page list

--- files/common.js
+++ files/common.js
@@ -21,6 +21,12 @@ $(function(){
 		$('.sub-menu').height(0);
 	});
 
+
+	$('#wm_alpha-menu li a').on('click',function(){
+		$('html,body').animate({scrollTop:$($(this).attr('href')).offset().top - 100},800);
+		return false;
+	});
+
 	$('#models-ticker').bxSlider({
 		maxSlides: 4,
 		slideWidth: 294,
@@ -49,12 +55,21 @@ $(function(){
 	});
 
 
-	$('.slides-wrapper').imageslider({
-		slideItems: '.my-slider-item',
-		slideContainer: '.js-slides',
-		slideDistance: 5,
-		slideDuratin: 800,
-		resizable: true,
-		pause: true
-	});
+	var wow = new WOW(
+	  {
+	    boxClass:     'wow',      // animated element css class (default is wow)
+	    animateClass: 'animated', // animation css class (default is animated)
+	    offset:       0,          // distance to the element when triggering the animation (default is 0)
+	    mobile:       false,       // trigger animations on mobile devices (default is true)
+	    live:         true,       // act on asynchronously loaded content (default is true)
+	    callback:     function(box) {
+	      // the callback is fired every time an animation is started
+	      // the argument that is passed in is the DOM node being animated
+	    },
+	    scrollContainer: null // optional scroll container selector, otherwise use window
+	  }
+	);
+	wow.init();
+
+
 });
